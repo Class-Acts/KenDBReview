95d0f751bfcdf555dd8218e780a4aa661b4443c6
Brian <bschaaf1017@gmail.com>
2021-01-28 16:45:24 -0500

    pw change

--- server/database/index.js
+++ server/database/index.js
@@ -4,7 +4,6 @@ const Promise = require('bluebird');
 var connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'password',
   database: 'rgi'
 });
 const db = Promise.promisifyAll(connection, { multiArgs: true });
